c96902b871f5e18d07c40d45a575392afc765c9c
lucas <lucaslima.94@hotmail.com.br>
2023-05-25 11:18:06 -0300

    função do menu

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TextEditor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello");
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("O que você deseja fazer?");
+        Console.WriteLine("1 - Abrir arquivo");
+        Console.WriteLine("1 - Criar arquivo");
+        Console.WriteLine("0 - Sair");
+
+        short option = short.Parse(Console.ReadLine()!);
+
+        switch (option)
+        {
+            case 1:
+                Abrir();
+                break;
+            case 2:
+                Editar();
+                break;
+            case 0:
+                System.Environment.Exit(0);
+                break;
+
+            default:
+                break;
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Menu();
+        }
+    }
+}
